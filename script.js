113495fd3dc53fcaa52f126350ba9f3d5b0984fb
snehakumari-37 <92073117+snehakumari-37@users.noreply.github.com>
2022-02-27 22:33:02 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,33 @@
+var badRequest = false;
+if (!window.location.search) { badRequest = true }
+
+var q = document.location.search.split('?')[1].split('.');
+var base = "https://ncert.nic.in/textbook/pdf/";
+var subCode; var end = q[1]; var sub = q[0]; var oneZero = "10";
+
+var bharaj_Ki_Khoj_Code = "h2";     var bharaj_Ki_Khoj = "hhbk";
+var vasant_3_Code = "h1";           var vasant_3 = "hhvs";
+var honeydew_Code = "e1";           var honeydew = "hehd";
+var it_So_Happened_Code = "e2";     var it_So_Happened = "heih";
+var mahts_Code = "m";               var maths = "hemh";
+var science_Code = "s";             var science = "hesc";
+var his_Code = "h";                 var sst = "hess";
+var civics_Code = "c";
+var geo_Code = "g"; 
+
+if (sub == bharaj_Ki_Khoj_Code)         { var subCode = bharaj_Ki_Khoj; }
+else if (sub == vasant_3_Code)          { var subCode = vasant_3; }
+else if (sub == honeydew_Code)          { var subCode = honeydew; }
+else if (sub == it_So_Happened_Code)    { var subCode = it_So_Happened; }
+else if (sub == mahts_Code)             { var subCode = maths; }
+else if (sub == science_Code)           { var subCode = science; }
+else if (sub == his_Code)               { var subCode = sst; oneZero = "20" }
+else if (sub == civics_Code)            { var subCode = sst; oneZero = "30" }
+else if (sub == geo_Code)               { var subCode = sst; oneZero = "40" }
+else { var badRequest = true; }
+
+if (end.length==2){var oneZero=oneZero.split("0")[0]; var full = base + subCode + oneZero + end + ".pdf";}
+else if (end == "0") { var end = "1ps"; var full = base + subCode + end + ".pdf"; }
+else {var full = base + subCode + oneZero + end + ".pdf";}
+
+if (!badRequest) { window.location.replace(full); }
